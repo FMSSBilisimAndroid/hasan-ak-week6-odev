52f4719087d4882d135c22b3936c06bacf51654e
Hasan Ak <hasan_ak_44@outlook.com>
2022-10-02 22:05:26 +0300

    Update MainActivity.kt

--- app/src/main/java/com/hasan/coroutinesapp/MainActivity.kt
+++ app/src/main/java/com/hasan/coroutinesapp/MainActivity.kt
@@ -13,20 +13,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var second = 0
-
+        
+        //infinite loop created.
         while (true) {
+            //the operation takes place on the IO thread.
             CoroutineScope(Dispatchers.IO).launch {
                 val answer = doNetwork()
-
+                //Observing on MainThread 
                 withContext(Dispatchers.Main) {
                     Log.v("PATIKA", answer)
                 }
             }
+            //The secound variable increments by one each time the loop returns.
             second+=1
             Log.v("SECOUND",second.toString())
         }
     }
-
+    //this function is used by coroutinescope and its task returns the specified string with 2 seconds delay. 
     suspend fun doNetwork():String{
         delay(2000L)
         return "Network Answer Called"
